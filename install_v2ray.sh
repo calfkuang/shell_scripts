18b26aa16e396703ca0ce0ed2bc2ea4c3953a1e8
tengfei <calfkuang@163.com>
2024-10-31 08:31:25 +0800

    随机端口

--- install_v2ray.sh
+++ install_v2ray.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #功能：自动安装v2ray服务端
-#      使用前先修改CLIENTS_ID值,示例：76555b48-78u6-660f-823f-f1433eff676e
 #作者：况腾飞
 #联系：calfkuang@163.com
 #TODO: 不同协议下配置参数不一样
 
 #配置基本变量
-PORT="1040"
+PORT=$(shuf -i 1-65535 -n1)
 CLIENTS_ID=$(uuidgen)
 PROTOCOL="vmess"
 V2RAY_CONFIG="/usr/local/etc/v2ray/config.json"
@@ -21,7 +20,7 @@ cat >${V2RAY_CONFIG} <<-EOF
     "inbounds": [{
         "port": "${PORT}",
         "listen": "0.0.0.0",
-        "tag": "socks-inbound",
+        "tag": "${PROTOCOL}-inbound",
         "protocol": "${PROTOCOL}",
         "settings": {
             "clients": [{
@@ -59,6 +58,7 @@ rm -f ${TMP_FILE}
 #打印安装信息
 echo -e "\e[31m---------------------V2RAY安装信息------------------------\e[0m"
 printf "\e[31m服务端口：\e[32m${PORT}\n"
+printf "\e[31mCLIENTS_ID：\e[32m${CLIENTS_ID}\n"
 printf "\e[31m配置文件：\e[32m${V2RAY_CONFIG}\n"
 printf "\e[31m运行状态：\e[32m${run_status}\n"
 echo -e "\e[31m----------------------------------------------------------\e[0m"
